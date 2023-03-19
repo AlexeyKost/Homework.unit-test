ba3541a362912a72f858c9a938e871fc0f975394
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-03-19 21:53:29 +0300

    Add files via upload

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoundaryAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHigherSum() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int expected = 800;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+}
